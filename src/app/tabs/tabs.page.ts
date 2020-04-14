411b098fbf57f57b2270c0ef2d741f6ce3386246
Noor Mohamed <noor.mohamed@macys.com>
2020-04-14 23:39:40 +0530

    bug fix

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -14,31 +14,31 @@ import { Platform } from '@ionic/angular';
 export class TabsPage {
 
   
-  @ViewChild(IonTabs)
-   tabs: IonTabs;
+  // @ViewChild(IonTabs)
+  //  tabs: IonTabs;
 
-   tabsList:any = [{
-    name: 'tab1',
-    label: 'Tab One',
-    icon: 'home'
-    }, {
-    name: 'tab2',
-    label: 'Tab Two',
-    icon: 'apps'
-    }, {
-    name: 'tab3',
-    label: 'Tab Three',
-    icon: 'send'
-  }];
-  pageTitle: any;
+  //  tabsList:any = [{
+  //   name: 'tab1',
+  //   label: 'Tab One',
+  //   icon: 'home'
+  //   }, {
+  //   name: 'tab2',
+  //   label: 'Tab Two',
+  //   icon: 'apps'
+  //   }, {
+  //   name: 'tab3',
+  //   label: 'Tab Three',
+  //   icon: 'send'
+  // }];
+  // pageTitle: any;
 
 constructor(private route: ActivatedRoute , public platform : Platform) {}
 
-setTitle() {
-  const currentTab: string = this.tabs.getSelected();
-  const matchingTab: any = this.tabsList.filter((tab: any) => tab.name === currentTab)[0]; //uses the first array element as the currentTab
-  this.pageTitle = matchingTab.label;
+// setTitle() {
+//   const currentTab: string = this.tabs.getSelected();
+//   const matchingTab: any = this.tabsList.filter((tab: any) => tab.name === currentTab)[0]; //uses the first array element as the currentTab
+//   this.pageTitle = matchingTab.label;
 
-}
+// }
 
 }
